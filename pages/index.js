2ad5cb7a162d1a86e8de274099b79521503661c9
Ronith Kandallu <ronithkandallu@gmail.com>
2023-01-13 17:15:25 -0800

    renamed “Request Access” button to “Request”

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ export default function Home() {
                     <br />
                     <br />
                     <Button href="https://forms.gle/QdLcaFMHoN6rdSfX8">
-                        Request Access
+                        Request
                     </Button>
                 </ContentContainer>
             </Screen>
